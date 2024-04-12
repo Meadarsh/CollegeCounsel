f8e1c96b5d19dc0f219a4d57269327fef3d71572
meadarsh <itsadarsh33@gmail.com>
2024-04-12 19:05:32 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
 app.use(cookieParser())
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://ccapi.collegecounsel.co.in',
     credentials: true,
   }));
   
